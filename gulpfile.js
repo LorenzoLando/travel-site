f7ddeea9d2c0fb692a8e72af394255ab26a470cf
Lorenzo <lorenzo.lando85@gmail.com>
2018-02-08 20:29:36 +0000

    new css organization

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'); //new way to write variables
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'); //new way to write variables
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task.");   //when you create a task the first one is the name the second the function
@@ -16,7 +17,7 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssvars, nested, autoprefixer]))
+  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
   .pipe(gulp.dest('./app/temp/styles'));
 });
 
